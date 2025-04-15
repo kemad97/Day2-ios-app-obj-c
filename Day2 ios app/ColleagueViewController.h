7cf4c6a13e04c80b8d6d37e33799ba1eeaa45aa8
kemad97 <kerolos.emad992@gmail.com>
2025-04-15 11:35:16 +0200

    add colleg h

---
+++ Day2 ios app/ColleagueViewController.h
@@ -0,0 +1,16 @@
+//
+//  ColleagueViewController.h
+//  Day2 ios app
+//
+//  Created by Kerolos on 15/04/2025.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ColleagueViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Day2 ios app/ColleagueViewController.m
@@ -0,0 +1,31 @@
+//
+//  ColleagueViewController.m
+//  Day2 ios app
+//
+//  Created by Kerolos on 15/04/2025.
+//
+
+#import "ColleagueViewController.h"
+
+@interface ColleagueViewController ()
+
+@end
+
+@implementation ColleagueViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
